0162fb5decf28a30aedec90fb89382d4df337bed
sabbirziauddin <sabbircseiiuc@gmail.com>
2020-03-25 02:24:31 +0100

    increament

---
+++ script.js
@@ -0,0 +1,19 @@
+const increaseBtn = document.getElementById("increaseNumber");
+increaseBtn.addEventListener("click",function(){
+const input =parseInt(document.getElementById("plusCounter").value) ;
+//const inputNumber = parseInt(input) ;
+document.getElementById("plusCounter").value = input +1;
+
+
+//input.value = parseInt(input.value ) +1;
+     
+
+
+
+
+      
+
+
+
+      
+})
